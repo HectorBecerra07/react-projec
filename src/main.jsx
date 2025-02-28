4a830822619fcd7a6c8c70ce2caa1259ef0ff115
HectorBecerra07 <rivehect5t@gmail.com>
2025-02-27 22:42:48 -0600

    correcion 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';  // Si tienes estilos globales
+import App from './App.jsx';  // El archivo JSX se importa sin problemas
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  ,
-)
+  </React.StrictMode>
+);
