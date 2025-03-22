03b81f671d1d7d2feb4a157e771094a5dd10fccd
Hector Becerra <rivehect5t@gmail.com>
2025-03-21 23:47:35 -0600

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style/index.css';  // Si tienes estilos globales
-import App from './App.jsx';  
-import 'bootstrap/dist/css/bootstrap.min.css';// El archivo JSX se importa sin problemas
+import './style/index.css';  
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
